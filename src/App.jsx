5fae95293dcaaf63df86675c7d7e5026b8820bb5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-19 23:26:53 +0000

    Move Course component and all Course subcomponents to a new file and import into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,7 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name}: {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((item) => (
-        <Part part={item} key={item.id} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ course }) => {
-  const arr = [];
-  for (let i = 0; i < course.parts.length; i++) {
-    arr.push(course.parts[i].exercises);
-  }
-
-  const init = 0;
-  const total = arr.reduce((acc, curr) => acc + curr, init);
-
-  return <p>Total of {total} exercises</p>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course}></Header>
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 function App() {
   const courses = [
